44a1daf054c8d3f1fd933ecc1c78a8fb2d3ff279
Liagush <48795444+Liagush@users.noreply.github.com>
2019-04-06 11:06:13 +0300

    filter base

--- src/main/java/com/example/test/Application.java
+++ src/main/java/com/example/test/Application.java
@@ -1,5 +1,9 @@
 package com.example.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +14,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
-
-
 }
--- src/main/java/com/example/test/repos/MessageRepo.java
+++ src/main/java/com/example/test/repos/MessageRepo.java
@@ -3,6 +3,9 @@ package com.example.test.repos;
 import com.example.test.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
 
+    List<Message> findByTag(String tag);
 }
