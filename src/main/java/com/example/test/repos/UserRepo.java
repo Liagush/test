dac2ae1e36481b627603ee7ff94c5dda867ce3cc
Liagush <48795444+Liagush@users.noreply.github.com>
2019-04-08 21:53:25 +0300

    freemarker + registration + autorization

---
+++ src/main/java/com/example/test/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.test.repos;
+
+import com.example.test.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ src/main/resources/templates/parts/common.ftl
@@ -0,0 +1,12 @@
+<#macro page>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Reshala</title>
+</head>
+<body>
+<#nested>
+</body>
+</html>
+</#macro>
